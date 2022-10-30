c9be6a70fc7b75dd5dce828a08a8b55548f86c26
Marcos Vinicius <marcos.vinciuswac@outlook.com>
2022-10-30 01:29:01 -0300

    Projeto Udemy, Xadrezão da massa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoXadrex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
