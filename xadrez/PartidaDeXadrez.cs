b055b99c805eb46ead87d1040291bf6ed764a7e3
Marcos Vinicius <marcos.vinciuswac@outlook.com>
2022-11-20 17:38:45 -0300

    Jogada especial promoção

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -201,6 +201,21 @@ namespace xadrez
                 throw new TabuleiroExeption("Você não pode se colocar em xeque!");
             }
             Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if (p is Peao) 
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7)) 
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama  = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            
+            }
+
             if (estaEmXeque(adversaria(JogadorAtual)))
             {
                 xeque = true;
